d62e88434defd18ed12fce456b65330118c53ad0
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-15 12:47:15 +0100

    adding the auth middleware | refactoring the code

--- controllers/main.js
+++ controllers/main.js
@@ -3,50 +3,44 @@
 //send back to front-end
 
 //setup authentication so only the request with JWT can access the dashboard
-const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
 
-const login = async ( req, res ) => {
-    const{username, password} = req.body
+const login = async (req, res) => {
+  const { username, password } = req.body;
 
-    
-    // mongoose validation
-    // Joi : validation layer with the help of Joi (package)
-    // check in the controller
+  // mongoose validation
+  // Joi : validation layer with the help of Joi (package)
+  // check in the controller
 
-    if(!username || !password){
-        throw new CustomAPIError('Please provide email and password',400)
-    }
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
 
-    // just for demo, normally provided by DB!!!
-    const id = new Date().getDate()
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getDate();
 
+  // try to keep payload samell, better experience for user
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 
-    // try to keep payload samell, better experience for user
-    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
-
-    res.status(200).json({msg:'user created',token})
-}
-
+  res.status(200).json({ msg: 'user created', token });
+};
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new CustomAPIError('No token provided', 401)
-    }
-
-    const token = authHeader.split(' ')[1]
-    try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
-        // console.log(decoded);
-        const luckyNumber = Math.floor(Math.random()*100)
-        res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-    } catch (error) {
-        throw new CustomAPIError('Not authorized to access this route',401)
-    }
-}
-
+  
+  
+  
+    console.log(req.user);
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
 
 module.exports = {
-    login,dashboard
-}
+  login,
+  dashboard,
+};
