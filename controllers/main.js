e9e572006697b0bb247c6336c7a776dcc9772ae0
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-15 11:25:35 +0100

    Understanding JWT

--- controllers/main.js
+++ controllers/main.js
@@ -1,7 +1,22 @@
+//check username, password in post(login) request
+// if exist create new JWT (Token)
+//send back to front-end
 
-
+//setup authentication so only the request with JWT can access the dashboard
+const CustomAPIError = require('../errors/custom-error')
 
 const login = async ( req, res ) => {
+    const{username, password} = req.body
+
+    
+    // mongoose validation
+    // Joi : validation layer with the help of Joi (package)
+    // check in the controller
+
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password',400)
+    }
+
     res.send('Fake Login/Register/Signup Route')
 }
 
